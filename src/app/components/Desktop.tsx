09402a5abeb39b5c73a9d91daf0ec6475864a11b
JhueiGitHub <matthewsheehan@me.com>
2025-04-13 14:02:32 +0100

    Desktop component update

---
+++ src/app/components/Desktop.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+type Props = {};
+
+const Desktop = (props: Props) => {
+  return (
+    <div className="w-full h-full bg-black flex items-center justify-center text-[#4C4F69]">
+      Desktop
+    </div>
+  );
+};
+
+export default Desktop;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
+import Desktop from "./components/Desktop";
+
 export default function Home() {
   return (
-    <div className="w-screen h-screen bg-black flex justify-center items-center">
-      <span className="text-[#4C4F69]">Desktop</span>
+    <div className="w-screen h-screen bg-black">
+      <Desktop />
     </div>
   );
 }
